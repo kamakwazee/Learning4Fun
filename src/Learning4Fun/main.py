9c0dbb3165a52c191d3a0fccee4122775755d6d7
Tabor Kvasnicka <kamakwazee@hotmail.com>
2013-07-30 12:05:06 -0500

    Added randomized letter
    Signed-off-by: Tabor Kvasnicka <kamakwazee@hotmail.com>

--- src/Learning4Fun/main.py
+++ src/Learning4Fun/main.py
@@ -1,1 +1,51 @@
 #Main File for Learning4Fun
+#
+#Created and maintained by Kamakwazee Games from Kamakwazee Organizations.
+#
+#
+
+import pygame, sys, Tkinter, random
+from pygame.locals import *
+from Tkinter import *
+from random import randint
+
+class Main:
+    def randomLetter(self, exclusion=None):
+        if exclusion is None:
+            return self.abc[randint(0,len(self.abc)-1)]
+        else:
+            l = self.abc[randint(0,len(self.abc)-1)]
+            if l == exclusion:
+                return self.randomLetter(exclusion)
+            else:
+                return l
+    def drawQuestion(self):
+        self.letter = self.randomLetter()
+    def declareVar(self):
+        self.white = (255,255,255)
+        root = Tk()
+        self.window_w = root.winfo_screenwidth()
+        self.window_h = root.winfo_screenheight()
+        self.screen = pygame.display.set_mode((self.window_w,self.window_h),FULLSCREEN,0)
+        self.abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','l','o','p','q','r','s','t','u','v','w','x','y','z']
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+    def __init__(self):
+        pygame.init()
+        #Initialize variables
+        self.declareVar()
+        self.drawQuestion()
+        while True:
+            self.events()
+            pygame.display.flip()
+
+Main()
+
+
+
+
+
